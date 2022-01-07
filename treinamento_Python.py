1cb765e6f9b685cb4ed4901eb6be9c39ee00f921
Bruno Kalata <bkalata@gmail.com>
2022-01-07 18:05:18 -0300

    Create treinamento_Python.py

---
+++ treinamento_Python.py
@@ -0,0 +1,25 @@
+nome = input('Qual seu nome? ')
+
+print ('Seja muito bem vindo ao meu primeiro programa em Python', nome)
+
+
+print ('================ DESAFIO 2 ================')
+
+
+dia = input ('Qual o dia do seu nascimento? ')
+mes = input ('Qual o mês do seu nascimento? ')
+ano = input ('Qual o ano que vc nasceu? ')
+
+
+print ('Que massa tu nasceu em', dia, mes, ano)
+
+
+
+print ('================ DESAFIO 3 ================')
+
+primeiro = input ('Digite o primeiro numero: ')
+segundo = input ('Segundo: ')
+resultado = int(primeiro) + int(segundo)
+
+print( 'A soma total é: ',resultado)
+
